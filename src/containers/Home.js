180f3d1374bf9b00412b3a966d2ad27bec60345b
github username <github email address>
2019-10-16 18:18:31 -0400

    new logo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = (props) => {
   return(
     <div className="home">
-      <img className="logo" src="https://fontmeme.com/permalink/191016/4108d4ce9cb41baa8d09c11814e46551.png"/>
+      <img className="logo" src="https://fontmeme.com/temporary/cbf1ed2f47f7d3fc7737891621640f3f.png"/>
       <div className="silhouette">
         <img className="bg-img" src="https://i.imgur.com/w9HSItk.jpg"/>
       </div>
